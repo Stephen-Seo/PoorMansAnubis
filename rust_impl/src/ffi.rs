bf58ddacd5c739cedf1965211ef51b205f9b8faa
Stephen Seo <seo.disparate@gmail.com>
2025-05-05 22:47:38 +0900

    Refactor Rust code handling work factors

--- rust_impl/src/ffi.rs
+++ rust_impl/src/ffi.rs
@@ -7,20 +7,48 @@ use std::ffi::{CStr, CString, c_void};
 
 include!(concat!(env!("OUT_DIR"), "/work_bindings.rs"));
 
-pub fn generate_value_and_factors_strings(digits: u64) -> Result<(CString, CString), String> {
-    let value: CString;
-    let factors: CString;
+struct WorkFactorsWrapper {
+    w_factors: Work_Factors,
+}
 
-    unsafe {
-        let mut work_factors = work_generate_target_factors(digits);
-        let value_ffi_cstr = work_factors_value_to_str(work_factors, std::ptr::null_mut());
-        value = CStr::from_ptr(value_ffi_cstr).to_owned();
-        libc::free(value_ffi_cstr as *mut c_void);
-        let factors_ffi_cstr = work_factors_factors_to_str(work_factors, std::ptr::null_mut());
-        factors = CStr::from_ptr(factors_ffi_cstr).to_owned();
-        libc::free(factors_ffi_cstr as *mut c_void);
-        work_cleanup_factors(&mut work_factors as *mut Work_Factors);
+impl WorkFactorsWrapper {
+    pub fn new(digits: u64) -> Self {
+        Self {
+            w_factors: unsafe { work_generate_target_factors(digits) },
+        }
     }
 
-    Ok((value, factors))
+    pub fn get_value(&self) -> CString {
+        let value: CString;
+        unsafe {
+            let value_ffi_cstr = work_factors_value_to_str(self.w_factors, std::ptr::null_mut());
+            value = CStr::from_ptr(value_ffi_cstr).to_owned();
+            libc::free(value_ffi_cstr as *mut c_void);
+        }
+        value
+    }
+
+    pub fn get_factors(&self) -> CString {
+        let factors: CString;
+        unsafe {
+            let factors_ffi_cstr =
+                work_factors_factors_to_str(self.w_factors, std::ptr::null_mut());
+            factors = CStr::from_ptr(factors_ffi_cstr).to_owned();
+            libc::free(factors_ffi_cstr as *mut c_void);
+        }
+        factors
+    }
+}
+
+impl Drop for WorkFactorsWrapper {
+    fn drop(&mut self) {
+        unsafe {
+            work_cleanup_factors(&mut self.w_factors as *mut Work_Factors);
+        }
+    }
+}
+
+pub fn generate_value_and_factors_strings(digits: u64) -> (CString, CString) {
+    let wf = WorkFactorsWrapper::new(digits);
+    (wf.get_value(), wf.get_factors())
 }
--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -2,7 +2,7 @@ mod ffi;
 
 fn main() {
     println!("Generating value and factors of 10 digits:");
-    let (v, f) = ffi::generate_value_and_factors_strings(10).unwrap();
+    let (v, f) = ffi::generate_value_and_factors_strings(10);
     println!("{:?}", v);
     println!("{:?}", f);
 }
