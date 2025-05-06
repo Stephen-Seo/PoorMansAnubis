3a41e833478eb155b0bac328261642135bec063a
Stephen Seo <seo.disparate@gmail.com>
2025-05-06 15:44:04 +0900

    rust_impl: Begin work as web frontend using salvo

--- rust_impl/src/args.rs
+++ rust_impl/src/args.rs
@@ -2,10 +2,14 @@ use std::env::args as args_fn;
 
 pub struct Args {
     pub factors: Option<u64>,
+    pub addr_port_str: String,
 }
 
 pub fn parse_args() -> Args {
-    let mut args = Args { factors: None };
+    let mut args = Args {
+        factors: None,
+        addr_port_str: "127.0.0.1:8180".into(),
+    };
 
     let p_args = args_fn();
 
@@ -13,6 +17,9 @@ pub fn parse_args() -> Args {
         if arg.starts_with("--factors=") {
             let end = arg.split_off(10);
             args.factors = end.parse().ok();
+        } else if arg.starts_with("--addr_port=") {
+            let end = arg.split_off(12);
+            args.addr_port_str = end.to_owned();
         }
     }
 
--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -1,9 +1,18 @@
 mod args;
 mod ffi;
 
+use salvo::prelude::*;
+
 const DEFAULT_FACTORS_DIGITS: u64 = 17000;
 
-fn main() {
+#[handler]
+async fn handler_fn(req: &mut Request, res: &mut Response) {
+    let path_str = req.uri().path_and_query().unwrap().as_str().to_owned();
+    res.render(Text::Plain(path_str));
+}
+
+#[tokio::main]
+async fn main() {
     let mut parsed_args = args::parse_args();
     if parsed_args.factors == None {
         parsed_args.factors = Some(DEFAULT_FACTORS_DIGITS);
@@ -12,11 +21,8 @@ fn main() {
             DEFAULT_FACTORS_DIGITS
         );
     }
-    println!(
-        "Generating value and factors of {} digits:",
-        parsed_args.factors.as_ref().unwrap()
-    );
-    let (v, f) = ffi::generate_value_and_factors_strings(*parsed_args.factors.as_ref().unwrap());
-    println!("{}", v);
-    println!("{}", f);
+
+    let router = Router::new().path("{**}").get(handler_fn);
+    let acceptor = TcpListener::new(&parsed_args.addr_port_str).bind().await;
+    Server::new(acceptor).serve(router).await;
 }
