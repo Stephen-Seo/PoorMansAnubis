4e19c513d115eb399c0728885c3df05a304c0ab5
Stephen Seo <seo.disparate@gmail.com>
2025-05-06 22:25:39 +0900

    rust_impl: Ensure api url accepts only POST

--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -15,8 +15,9 @@ async fn req_to_url(
 }
 
 #[handler]
-async fn api_fn(depot: &mut Depot, res: &mut Response) {
+async fn api_fn(depot: &mut Depot, req: &mut Request, res: &mut Response) {
     let _args = depot.obtain::<args::Args>().unwrap();
+    eprintln!("API: {}", req.uri().path_and_query().unwrap());
     res.render("API");
 }
 
@@ -56,7 +57,7 @@ async fn main() {
 
     let router = Router::new()
         .hoop(affix_state::inject(parsed_args.clone()))
-        .push(Router::new().path("/pma_api").get(api_fn))
+        .push(Router::new().path("/pma_api").post(api_fn))
         .push(Router::new().path("{**}").get(handler_fn));
     let acceptor = TcpListener::new(&parsed_args.addr_port_str).bind().await;
     Server::new(acceptor).serve(router).await;
