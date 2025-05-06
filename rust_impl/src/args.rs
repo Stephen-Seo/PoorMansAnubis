af6d44bea1026b7fe26a9206f22ea0797abe670e
Stephen Seo <seo.disparate@gmail.com>
2025-05-06 16:18:35 +0900

    rust_impl: Render "redirect" to dest

--- rust_impl/src/args.rs
+++ rust_impl/src/args.rs
@@ -1,13 +1,16 @@
 use std::env::args as args_fn;
 
+#[derive(Default, Clone, Debug)]
 pub struct Args {
     pub factors: Option<u64>,
+    pub dest_url: String,
     pub addr_port_str: String,
 }
 
 pub fn parse_args() -> Args {
     let mut args = Args {
         factors: None,
+        dest_url: "https://git.seodisparate.com".into(),
         addr_port_str: "127.0.0.1:8180".into(),
     };
 
@@ -17,7 +20,10 @@ pub fn parse_args() -> Args {
         if arg.starts_with("--factors=") {
             let end = arg.split_off(10);
             args.factors = end.parse().ok();
-        } else if arg.starts_with("--addr_port=") {
+        } else if arg.starts_with("--dest-url=") {
+            let end = arg.split_off(11);
+            args.dest_url = end.to_owned();
+        } else if arg.starts_with("--addr-port=") {
             let end = arg.split_off(12);
             args.addr_port_str = end.to_owned();
         }
--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -1,14 +1,30 @@
 mod args;
 mod ffi;
 
-use salvo::prelude::*;
+use salvo::{http::ResBody, prelude::*};
 
 const DEFAULT_FACTORS_DIGITS: u64 = 17000;
 
+async fn req_to_url(url: String) -> Result<(ResBody, u16), reqwest::Error> {
+    let req = reqwest::get(url).await?;
+    let status = req.status().as_u16();
+    Ok((ResBody::Once(req.bytes().await?), status))
+}
+
 #[handler]
-async fn handler_fn(req: &mut Request, res: &mut Response) {
+async fn handler_fn(depot: &mut Depot, req: &mut Request, res: &mut Response) {
+    let args = depot.obtain::<args::Args>().unwrap();
+
     let path_str = req.uri().path_and_query().unwrap().as_str().to_owned();
-    res.render(Text::Plain(path_str));
+
+    let res_body_res = req_to_url(format!("{}{}", args.dest_url, &path_str)).await;
+    if let Ok((res_body, status)) = res_body_res {
+        res.replace_body(res_body);
+        res.status_code = Some(StatusCode::from_u16(status).unwrap());
+    } else {
+        res.render("Failed to query");
+        res.status_code = Some(StatusCode::INTERNAL_SERVER_ERROR);
+    }
 }
 
 #[tokio::main]
@@ -21,8 +37,13 @@ async fn main() {
             DEFAULT_FACTORS_DIGITS
         );
     }
+    println!("URL: {}", &parsed_args.dest_url);
+    println!("Listening: {}", &parsed_args.addr_port_str);
 
-    let router = Router::new().path("{**}").get(handler_fn);
+    let router = Router::new()
+        .hoop(affix_state::inject(parsed_args.clone()))
+        .path("{**}")
+        .get(handler_fn);
     let acceptor = TcpListener::new(&parsed_args.addr_port_str).bind().await;
     Server::new(acceptor).serve(router).await;
 }
