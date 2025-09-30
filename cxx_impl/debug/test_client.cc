36db2bc3beef567c0c68c6355833526ffb0eac0d
Stephen Seo <seo.disparate@gmail.com>
2025-09-30 12:17:03 +0900

    debug/test_client: Impl. printing response

--- cxx_impl/debug/test_client.cc
+++ cxx_impl/debug/test_client.cc
@@ -5,6 +5,7 @@
 #include <thread>
 
 #include <unistd.h>
+#include <poll.h>
 
 void print_usage() {
   std::println("./program ( --cli-ipv4=0.0.0.0 | --cli-ipv6=:: ) ( --ser-ipv4=0.0.0.0 | --ser-ipv6=:: ) --port=9000");
@@ -61,6 +62,8 @@ int main(int argc, char **argv) {
     return 0;
   }
 
+  // Do write.
+  std::println("Start write request");
   while (true) {
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
     ssize_t ret_write = write(socket_fd, "GET / HTTP/1.1\n\n", 16);
@@ -69,6 +72,7 @@ int main(int argc, char **argv) {
         continue;
       } else {
         close(socket_fd);
+        socket_fd = -1;
         std::println(stderr, "ERROR: Failed to write to socket, errno {}", errno);
         break;
       }
@@ -77,8 +81,52 @@ int main(int argc, char **argv) {
     }
   }
 
+  // Read response.
+  std::println("Start read response");
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    struct pollfd pfd = {socket_fd, POLLIN, 0};
+    int ret = poll(&pfd, 1, /* milliseconds */ 1000);
+    if (ret == -1) {
+      close(socket_fd);
+      socket_fd = -1;
+      std::println("Error while polling socket_fd, errno {}", errno);
+      break;
+    } else if ((pfd.revents & POLLIN) != 0) {
+      std::println("POLLIN");
+      std::array<char, 1024> buf{0};
+      ssize_t read_ret = read(socket_fd, buf.data(), buf.size() - 1);
+      if (read_ret == -1) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+          continue;
+        } else {
+          close(socket_fd);
+          socket_fd = -1;
+          std::println("Error reading from socket, errno {}", errno);
+          break;
+        }
+      } else if (read_ret > 0) {
+        buf.at(read_ret) = 0;
+        std::println("Read: {}", buf.data());
+      } else if (read_ret == 0) {
+        std::println("EOF");
+        close(socket_fd);
+        socket_fd = -1;
+        break;
+      }
+    } else if ((pfd.revents & POLLHUP) != 0 || (pfd.revents & POLLERR) != 0) {
+      std::println("POLLHUP | POLLERR");
+      close(socket_fd);
+      socket_fd = -1;
+      std::println("Notice connection closed by other side");
+      break;
+    }
+  }
+
   std::println("End of loop...");
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  close(socket_fd);
+  if (socket_fd >= 0) {
+    close(socket_fd);
+  }
   return 0;
 }
