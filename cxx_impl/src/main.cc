196164332c31ce7fce123cae8ca9b818d91b861e
Stephen Seo <seo.disparate@gmail.com>
2025-10-14 18:11:30 +0900

    cxx_backend main: more refactoring of info holding
    Uses the AddrInfo tuple with a bitset (flags).

--- cxx_impl/src/main.cc
+++ cxx_impl/src/main.cc
@@ -15,9 +15,11 @@
 // PERFORMANCE OF THIS SOFTWARE.
 
 // Standard library includes
+#include <bitset>
 #include <chrono>
 #include <optional>
 #include <thread>
+#include <tuple>
 #include <unordered_map>
 
 // Unix includes
@@ -42,6 +44,19 @@ void receive_signal(int sig) {
   interrupt_received = 1;
 }
 
+// Bitset flags:
+// 0 - is ipv4
+using AddrPortInfo = std::tuple<std::string, uint16_t, std::bitset<16>>;
+
+AddrPortInfo conv_addr_port(const PMA_ARGS::AddrPort &addr_port, bool is_ipv4) {
+  auto tuple = std::make_tuple(std::get<0>(addr_port), std::get<1>(addr_port),
+                               std::bitset<16>{});
+
+  std::get<2>(tuple).set(0, is_ipv4);
+
+  return tuple;
+}
+
 int main(int argc, char **argv) {
   const PMA_ARGS::Args args(argc, argv);
 
@@ -51,9 +66,9 @@ int main(int argc, char **argv) {
   }
 
   // Mapping is a socket-fd to port
-  std::unordered_map<int, PMA_ARGS::AddrPort> sockets;
-  GenericCleanup<std::unordered_map<int, PMA_ARGS::AddrPort> *> cleanup_sockets(
-      &sockets, [](std::unordered_map<int, PMA_ARGS::AddrPort> **s) {
+  std::unordered_map<int, AddrPortInfo> sockets;
+  GenericCleanup<std::unordered_map<int, AddrPortInfo> *> cleanup_sockets(
+      &sockets, [](std::unordered_map<int, AddrPortInfo> **s) {
         PMA_Println("Cleaning up sockets...");
         for (auto iter = (*s)->begin(); iter != (*s)->end(); ++iter) {
           if (iter->first >= 0) {
@@ -64,15 +79,18 @@ int main(int argc, char **argv) {
 
   for (const PMA_ARGS::AddrPort &a : args.addr_ports) {
     std::optional<int> socket_fd_opt;
+    bool is_ipv4;
     const auto [err, msg, socket_fd] =
         PMA_HTTP::get_ipv6_socket_server(std::get<0>(a), std::get<1>(a));
     if (err == PMA_HTTP::ErrorT::SUCCESS) {
       socket_fd_opt = socket_fd;
+      is_ipv4 = false;
     } else {
       const auto [err, msg, socket_fd] =
           PMA_HTTP::get_ipv4_socket_server(std::get<0>(a), std::get<1>(a));
       if (err == PMA_HTTP::ErrorT::SUCCESS) {
         socket_fd_opt = socket_fd;
+        is_ipv4 = true;
       } else {
         PMA_EPrintln(
             "ERROR: Failed to get listening socket for addr \"{}\" on port "
@@ -83,7 +101,7 @@ int main(int argc, char **argv) {
     }
 
     if (socket_fd_opt.has_value() && socket_fd_opt.value() >= 0) {
-      sockets.emplace(socket_fd_opt.value(), a);
+      sockets.emplace(socket_fd_opt.value(), conv_addr_port(a, is_ipv4));
       PMA_Println("Listening on {}:{}", std::get<0>(a), std::get<1>(a));
     } else {
       PMA_EPrintln(
@@ -99,9 +117,9 @@ int main(int argc, char **argv) {
   }
 
   // Mapping is a connection-fd to socket-fd
-  std::unordered_map<int, int> connections;
-  GenericCleanup<std::unordered_map<int, int> *> cleanup_connections(
-      &connections, [](std::unordered_map<int, int> **s) {
+  std::unordered_map<int, AddrPortInfo> connections;
+  GenericCleanup<std::unordered_map<int, AddrPortInfo> *> cleanup_connections(
+      &connections, [](std::unordered_map<int, AddrPortInfo> **s) {
         PMA_Println("Cleaning up connections...");
         for (auto iter = (*s)->begin(); iter != (*s)->end(); ++iter) {
           if (iter->first >= 0) {
@@ -117,6 +135,9 @@ int main(int argc, char **argv) {
   const auto sleep_duration = std::chrono::milliseconds(SLEEP_MILLISECONDS);
   while (!interrupt_received) {
     std::this_thread::sleep_for(sleep_duration);
+    // TODO: Receive connections from sockets and handle each connection here.
+    // Also figure out fetching the addr of the incoming connection.
+    // Probably will use the output-parameters of "accept()" to get this.
   }
 
   return 0;
