ea1c94e38ce2d5cb54d876798aebf7f7640dda5c
Stephen Seo <seo.disparate@gmail.com>
2025-12-31 16:03:31 +0900

    Fix rust_impl/cross_build_to_aarch64.sh

--- rust_impl/build.rs
+++ rust_impl/build.rs
@@ -3,6 +3,13 @@ use std::path::PathBuf;
 
 fn main() {
     cc::Build::new()
+        .cpp(true)
+        .cpp_link_stdlib_static(true)
+        .file("../challenge_impl/src/work2.cc")
+        .include("../challenge_impl/third_party/SimpleArchiver/src")
+        .compile("cpp_work");
+
+    cc::Build::new()
         .file("../challenge_impl/src/work.c")
         .file("../challenge_impl/src/base64.c")
         .file("../challenge_impl/third_party/SimpleArchiver/src/data_structures/linked_list.c")
@@ -10,13 +17,6 @@ fn main() {
         .file("../challenge_impl/third_party/SimpleArchiver/src/data_structures/priority_heap.c")
         .include("../challenge_impl/third_party/SimpleArchiver/src")
         .compile("c_work");
-
-    cc::Build::new()
-        .cpp(true)
-        .cpp_link_stdlib_static(true)
-        .file("../challenge_impl/src/work2.cc")
-        .include("../challenge_impl/third_party/SimpleArchiver/src")
-        .compile("cpp_work");
 
     let bindings = bindgen::Builder::default()
         .clang_arg("-I../challenge_impl/third_party/SimpleArchiver/src")
--- rust_impl/cross_build_to_aarch64.sh
+++ rust_impl/cross_build_to_aarch64.sh
@@ -3,12 +3,13 @@
 set -ve
 
 CC_aarch64_linux_gnu=/usr/bin/aarch64-linux-gnu-gcc
+CXX_aarch64_linux_gnu=/usr/bin/aarch64-linux-gnu-g++
 
 /usr/bin/env \
     CC_aarch64_linux_gnu=${CC_aarch64_linux_gnu} \
-    CXX_aarch64_linux_gnu=/usr/bin/aarch64-linux-gnu-g++ \
+    CXX_aarch64_linux_gnu=${CXX_aarch64_linxu_gnu} \
     AR_aarch64_linux_gnu=/usr/bin/aarch64-linux-gnu-ar \
     RANLIB_aarch64_linux_gnu=/usr/bin/aarch64-linux-gnu-ranlib \
     LD_aarch64_linux_gnu=/usr/bin/aarch64-linux-gnu-ld \
-    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CC_aarch64_linux_gnu}" \
+    CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER="${CXX_aarch64_linux_gnu}" \
     cargo build --target aarch64-unknown-linux-gnu "$@"
